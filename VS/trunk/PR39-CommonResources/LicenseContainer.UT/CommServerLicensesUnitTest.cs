ae1e049fe702c2684d9817d26202d0d3b9881103
mzbrzezny <mzbrzezny@8a3894fd-0c01-b944-991b-6a3da02cae8a>
2011-03-22 22:06:37 +0000

    [UA-2597] CRSC: Add possibility to test licenses - new test project is created to perform test on licenses Issue #: 2597

---
+++ VS/trunk/PR39-CommonResources/LicenseContainer.UT/CommServerLicensesUnitTest.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+using System.Reflection;
+using CAS.Lib.RTLib.Utils;
+
+namespace LicenseContainer.UT
+{
+  /// <summary>
+  /// Summary description for UnitTest1
+  /// </summary>
+  [TestClass]
+  public class CommServerLicensesUnitTest
+  {
+    public CommServerLicensesUnitTest()
+    {
+      //
+      // TODO: Add constructor logic here
+      //
+    }
+
+    private TestContext testContextInstance;
+
+    /// <summary>
+    ///Gets or sets the test context which provides
+    ///information about and functionality for the current test run.
+    ///</summary>
+    public TestContext TestContext
+    {
+      get
+      {
+        return testContextInstance;
+      }
+      set
+      {
+        testContextInstance = value;
+      }
+    }
+
+    #region Additional test attributes
+    //
+    // You can use the following additional attributes as you write your tests:
+    //
+    // Use ClassInitialize to run code before running the first test in the class
+    // [ClassInitialize()]
+    // public static void MyClassInitialize(TestContext testContext) { }
+    //
+    // Use ClassCleanup to run code after all tests in a class have run
+    // [ClassCleanup()]
+    // public static void MyClassCleanup() { }
+    //
+    // Use TestInitialize to run code before running each test 
+    // [TestInitialize()]
+    // public void MyTestInitialize() { }
+    //
+    // Use TestCleanup to run code after each test has run
+    // [TestCleanup()]
+    // public void MyTestCleanup() { }
+    //
+    #endregion
+
+    [TestMethod]
+    public void Install_All_CommServer_Licenses()
+    {
+      //
+      // TODO: Add test logic	here
+      //
+      string template_class;
+      using ( StreamReader sr1 = new StreamReader( Assembly.GetExecutingAssembly().GetManifestResourceStream( "LicenseContainer.UT.LicenseTester.cs" ) ) )
+      {
+        template_class = sr1.ReadToEnd();
+      }
+
+      using ( StreamReader sr2 = new StreamReader( Assembly.GetExecutingAssembly().GetManifestResourceStream( "LicenseContainer.UT.CommServer.csv" ) ) )
+      {
+        string line;
+        int linenumber = 0;
+        string[] header0 = null;
+        string[] header1 = null;
+        string[] elements;
+        do
+        {
+          line = sr2.ReadLine();
+          if ( line == null )
+            break; //end of strem
+          elements = line.Split( ';' );
+          if ( linenumber == 0 )
+            header0 = elements;
+          else if ( linenumber == 1 )
+            header1 = elements;
+          if ( linenumber > 1 )
+          {
+            CAS.Lib.CodeProtect.LibInstaller.InstalLicense( "TestUser", "CAS", "techsupp@cas.eu", true, "CommServer", elements[ 1 ] );
+            for ( int i = 2; i < header1.Length; i++ )
+            {
+              Guid guid = Guid.Empty;
+              try { guid = new Guid( header1[ i ] ); }
+              catch { }
+              if ( !guid.Equals( Guid.Empty ) )
+              {
+                int expected = int.Parse( elements[ i ] );
+                string my_class = template_class.Replace( "//%%GuidAttribute", string.Format( "[GuidAttribute( \"{0}\" )]", header1[ i ] ) );
+                CSharpStreamCompiller cssc = new CSharpStreamCompiller( my_class, new string[] { "CAS.CodeProtect.dll" } );
+                bool succeded_actual = true;
+                try
+                {
+                  object o = cssc.CompiledAssembly.CreateInstance( "LicenseContainer.UT.LicenseTester" );
+                }
+                catch
+                {
+                  succeded_actual = false;
+                }
+                Assert.AreEqual( expected > 0, succeded_actual, string.Format( "license test has failed!, Product: {0}, Function: {1}", elements[ 0 ], header0[ i ] ) );
+              }
+            }
+          }
+          linenumber++;
+        } while ( true );
+      }
+      CAS.Lib.CodeProtect.LibInstaller.InstalLicense( "TestUser", "CAS", "techsupp@cas.eu", true, "CommServer", "DefaultDemo" );
+    }
+  }
+}
---
+++ VS/trunk/PR39-CommonResources/LicenseContainer.UT/LicenseTester.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Runtime.InteropServices;
+using System.ComponentModel;
+using CAS.Lib.CodeProtect;
+using CAS.Lib.CodeProtect.LicenseDsc;
+
+namespace LicenseContainer.UT
+{
+  //%%GuidAttribute
+  [LicenseProvider( typeof( CodeProtectLP ) )]
+  sealed internal class LicenseTester: IsLicensed<LicenseTester>
+  {
+    /// <summary>
+    /// It is called by the constructor at the end of the license validation process if a proper license file can be opened.
+    /// If implemented by the derived class the current license can be used to get more information from it, e.g. <typeparamref name="Warning"/>.
+    /// </summary>
+    /// <param name="license">The current license for temporal use. The current license is disposed just after returning from this method</param>
+    /// <remarks>
+    /// Because the current license is disposed in the base class just after returning from this method, it must not be
+    /// assigned to locally to keep it for future use. To keep it for future use the derived class must make a local copy.
+    /// </remarks>
+    protected override void TraceCurrentLicence( LicenseFile license )
+    {
+      base.TraceCurrentLicence( license );
+      string msg = string.Format
+        ( "Obtained valid license for {0}, runtime={1}, volume={2}", typeof( LicenseTester ), license.RunTimeConstrain, license.VolumeConstrain );
+      Console.WriteLine( msg);
+    }
+    protected override void TraceFailureReason( string reason )
+    {
+      base.TraceFailureReason( reason );
+      throw new LicenseException( this.GetType(), this, reason );
+    }
+    protected override void TraceNoLicenseFile( string reason )
+    {
+      base.TraceNoLicenseFile( reason );
+      throw new LicenseException( this.GetType(), this, "No license file found, because: " + reason );
+    }
+    public LicenseTester()
+      : base( int.MaxValue, TimeSpan.MaxValue ) { }
+  }
+}
