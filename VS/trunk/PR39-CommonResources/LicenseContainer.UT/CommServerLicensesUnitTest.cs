dbf21d7acd7af98ea9a27689edc56c37f7f1484c
mzbrzezny <mzbrzezny@8a3894fd-0c01-b944-991b-6a3da02cae8a>
2011-03-24 10:32:33 +0000

    [UA-2597] CRSC: Add possibility to test licenses - cleanup and code refactoring - no changes to logic
    Issue #: 2597

--- VS/trunk/PR39-CommonResources/LicenseContainer.UT/CommServerLicensesUnitTest.cs
+++ VS/trunk/PR39-CommonResources/LicenseContainer.UT/CommServerLicensesUnitTest.cs
@@ -80,20 +80,14 @@ namespace LicenseContainer.UT
     [TestMethod]
     public void Test_All_CommServer_Licenses()
     {
-      //
-      // TODO: Add test logic	here
-      //
       PerformTestOnProduct( "CommServer" );
     }
+
     [TestMethod]
     public void Test_All_DataPorter_Licenses()
     {
-      //
-      // TODO: Add test logic	here
-      //
       PerformTestOnProduct( "DataPorter" );
     }
-
 
     private void PerformTestOnProduct( string productName )
     {
@@ -116,56 +110,66 @@ namespace LicenseContainer.UT
           else if ( linenumber == 1 )
           {
             header1 = elements;
-            GuidInstanceAndTestDefinition singleTestDefinition = null;
-            for ( int i = 2; i < header1.Length; i++ )
-            {
-              Guid guid = Guid.Empty;
-              try { guid = new Guid( header1[ i ] ); }
-              catch { }
-              if ( !guid.Equals( Guid.Empty ) )
-              {
-                singleTestDefinition = new GuidInstanceAndTestDefinition( guid, header0[ i ], i );
-                tests.Add( singleTestDefinition );
-              }
-              else
-                if ( singleTestDefinition != null )
-                {
-                  singleTestDefinition.TestDictionary.Add( i, header1[ i ] );
-                }
-            }
+            PrepareTestDefinitions( header0, header1, tests );
           }
           if ( linenumber > 1 )
           {
-            try
-            {
-              CAS.Lib.CodeProtect.LibInstaller.InstalLicense( "TestUser", "CAS", "techsupp@cas.eu", true, productName, elements[ 1 ] );
-            }
-            catch ( Exception ex )
-            {
-              Assert.Fail( string.Format( "Cannot install license {0} {1} (reason: {2})", elements[ 0 ], elements[ 1 ], ex.Message ) );
-            }
-            foreach ( GuidInstanceAndTestDefinition giatd in tests )
-            {
-              int expected = int.Parse( elements[ giatd.Index ] );
-              bool succeded_actual = true;
-              IIsLicensed o = null;
-              try
-              {
-                o = (IIsLicensed)giatd.CompiledAssembly.CreateInstance( "LicenseContainer.UT.LicenseTester" );
-              }
-              catch
-              {
-                succeded_actual = false;
-              }
-              if ( succeded_actual && o != null )
-                succeded_actual = o.Licensed;
-              string productAndFunctionInfo = string.Format( "Product: {0}, Function: {1}", elements[ 0 ], header0[ giatd.Index ] );
-              Assert.AreEqual( expected > 0, succeded_actual, string.Format( "license test has failed! {0}", productAndFunctionInfo ) );
-              Debug.WriteLine( string.Format( "Passed: {0}", productAndFunctionInfo ) );
-            }
+            PerformAllTestsOnOneLicense( productName, header0, elements, tests );
           }
           linenumber++;
         } while ( true );
+      }
+    }
+
+    private static void PerformAllTestsOnOneLicense( string productName, string[] header0, string[] elements, List<GuidInstanceAndTestDefinition> tests )
+    {
+      try
+      {
+        CAS.Lib.CodeProtect.LibInstaller.InstalLicense( "TestUser", "CAS", "techsupp@cas.eu", true, productName, elements[ 1 ] );
+      }
+      catch ( Exception ex )
+      {
+        Assert.Fail( string.Format( "Cannot install license {0} {1} (reason: {2})", elements[ 0 ], elements[ 1 ], ex.Message ) );
+      }
+      foreach ( GuidInstanceAndTestDefinition giatd in tests )
+      {
+        int expected = int.Parse( elements[ giatd.Index ] );
+        bool succeded_actual = true;
+        IIsLicensed o = null;
+        try
+        {
+          o = (IIsLicensed)giatd.CompiledAssembly.CreateInstance( "LicenseContainer.UT.LicenseTester" );
+        }
+        catch
+        {
+          succeded_actual = false;
+        }
+        if ( succeded_actual && o != null )
+          succeded_actual = o.Licensed;
+        string productAndFunctionInfo = string.Format( "Product: {0}, Function: {1}", elements[ 0 ], header0[ giatd.Index ] );
+        Assert.AreEqual( expected > 0, succeded_actual, string.Format( "license test has failed! {0}", productAndFunctionInfo ) );
+        Debug.WriteLine( string.Format( "Passed: {0}", productAndFunctionInfo ) );
+      }
+    }
+
+    private static void PrepareTestDefinitions( string[] header0, string[] header1, List<GuidInstanceAndTestDefinition> tests )
+    {
+      GuidInstanceAndTestDefinition singleTestDefinition = null;
+      for ( int i = 2; i < header1.Length; i++ )
+      {
+        Guid guid = Guid.Empty;
+        try { guid = new Guid( header1[ i ] ); }
+        catch { }
+        if ( !guid.Equals( Guid.Empty ) )
+        {
+          singleTestDefinition = new GuidInstanceAndTestDefinition( guid, header0[ i ], i );
+          tests.Add( singleTestDefinition );
+        }
+        else
+          if ( singleTestDefinition != null )
+          {
+            singleTestDefinition.TestDictionary.Add( i, header1[ i ] );
+          }
       }
     }
   }
