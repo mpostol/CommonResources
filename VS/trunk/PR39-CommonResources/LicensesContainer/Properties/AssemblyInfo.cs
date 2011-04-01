5d7ab9f68819caa433e56b3d311652dc29fbbef6
mzbrzezny <mzbrzezny@8a3894fd-0c01-b944-991b-6a3da02cae8a>
2011-04-01 12:53:59 +0000

    [UA-2650] CRSC: Release new version with fix for: OPCViewer - demo license is not working (wrong version constraint) - OPCViewer default license is fixed - version of Assembly is changed
    Issue #: 2650

--- VS/trunk/PR39-CommonResources/LicensesContainer/Properties/AssemblyInfo.cs
+++ VS/trunk/PR39-CommonResources/LicensesContainer/Properties/AssemblyInfo.cs
@@ -33,6 +33,6 @@ using System.Diagnostics.CodeAnalysis;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion( "1.0.0.*" )]
-[assembly: AssemblyFileVersion( "1.0.0.0" )]
+[assembly: AssemblyVersion( "1.00.01.*" )]
+[assembly: AssemblyFileVersion( "1.00.01.0" )]
 [assembly: SuppressMessage( "Microsoft.Design", "CA2210:AssembliesShouldHaveValidStrongNames", Justification = "mpostol reviewed" )]
