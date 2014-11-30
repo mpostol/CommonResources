d2a71267537dd986dc3c92470af766aa6234d027
mpostol <mpostol@8a3894fd-0c01-b944-991b-6a3da02cae8a>
2014-11-30 19:56:10 +0000

    Update versions CMCORE 5.0 - added AssemblyInfo.t4 template to the PR39-CommonResources.

---
+++ VS/trunk/PR39-CommonResources/T4Definitions/AssemblyInfo.cs
@@ -0,0 +1,34 @@
+﻿//<summary>
+//  Title   : Assembly info for: <Define AssemblycTitle>
+//  System  : Microsoft Visual C# .NET
+//  $LastChangedDate: 2014-10-17 23:06:15 +0200 (Pt, 17 paź 2014) $
+//  $Rev:  $
+//  $LastChangedBy:  $
+//  $URL:  $
+//  $Id:  $
+//
+//  Copyright (c) 2000-2014 CAS LODZ POLAND
+//  +48 (42) 686 25 47
+//  techsupp@cas.eu
+//  www.cas.eu
+//</summary>
+
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("<Define AssemblycTitle>")]
+[assembly: AssemblyDescription("<DefaultProductName>: <Define AssemblycTitle>")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("CAS")]
+[assembly: AssemblyProduct("<DefaultProductName>")]
+[assembly: AssemblyCopyright("Copyright (c) 2000-2014 CAS LODZ POLAND")]
+[assembly: AssemblyTrademark("<DefaultProductTrademark>")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("9f3155f3-599c-4a1c-b3fb-16e8cc305051")]
+[assembly: AssemblyVersion("1.00.00.*")]
+[assembly: AssemblyFileVersion("1.00.00")]
+
